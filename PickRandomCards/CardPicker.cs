04414329e4476769096b80358dbd563693443a0e
etienneferland-micha <etienneferland-micha@TIENNEFERLAA229>
2024-08-06 22:13:13 -0400

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    internal class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i =0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RamdomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RamdomSuit()
+        {
+            int value = random.Next(1, 5);
+            //if value = 1 return string to spades
+            if (value == 1) return "spades ♠️";
+            //if value = 2 return string to hearts
+            if (value == 2) return "hearts ♥";
+            //if value = 3 return string to clubs
+            if (value == 3) return "clubs ♣";
+            //if haven't return yet, return the string diamonds
+            return "diamonds♦";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+
+            if (value == 1) return "ace 🂡";
+            if (value == 11) return "jack 🂫";
+            if (value == 12) return "Queen 🂭";
+            if (value == 13) return "king 🂮";
+            return value.ToString();
+        }
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace PickRandomCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of cards to pick");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberoOfCards))
+            {
+                string[] pickedCards = CardPicker.PickSomeCards(numberoOfCards);
+                foreach(string card in pickedCards)
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid number or GET OUT!!!");
+            }
+        }        
+    }
+}
